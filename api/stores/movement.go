df605a580b5db755afeac5c8eb89db025b51ef5c
dzeleniak <zeleniak.david@gmail.com>
2023-09-25 21:52:55 -0700

    Create stores

---
+++ api/stores/movement.go
@@ -0,0 +1,96 @@
+package stores
+
+import (
+	"database/sql"
+
+	"github.com/dzeleniak/arnold/models"
+)
+
+type (
+	MovementStore interface {
+		Get(tx *sql.Tx) ([]models.Movement, error)
+		Create(tx *sql.Tx, movement *models.Movement) (int64, error)
+		UpdateById(tx *sql.Tx, movement *models.Movement)
+		DeleteById(tx *sql.Tx, id int) error
+	}
+
+	movementStore struct {
+		*sql.DB
+	}
+)
+
+func (s *movementStore) Get(tx *sql.Tx) ([]models.Movement, error) {
+	movements := make([]models.Movement, 0);
+	rows, err := s.Query("SELECT * FROM movement;")
+
+	if err != nil {
+		return nil, err;
+	}
+
+	for rows.Next() {
+		var m models.Movement;
+		err = rows.Scan(&m.ID, &m.Name);
+
+		if  err != nil {
+			return nil, err;
+		}
+		
+		movements = append(movements, m);
+	}
+
+	return movements, nil;
+}
+
+func (s *movementStore) Create(tx *sql.Tx, movement *models.Movement) (int64, error) {
+	var err error
+
+	query := "INSERT INTO movements (name) VALUES ($1) RETURNING id"
+
+	var id int64
+
+	if tx != nil {
+		err = tx.QueryRow(query, movement.Name).Scan(&id);
+	} else {
+		err = s.QueryRow(query, movement.Name).Scan(&id);
+	}
+
+	if err != nil {
+		return 0, err;
+	}
+
+	return id, nil;
+} 
+
+func (s *movementStore) UpdateById(tx *sql.Tx, movement *models.Movement) (int64, error) {
+	query, err := s.Prepare("UPDATE movements SET name = $1 WHERE movements.id = $2")
+	if err != nil {
+		return 0, err;
+	}
+
+	var id int64;
+
+	err = query.QueryRow(movement.Name).Scan(&id);
+
+	if id == 0 {
+		return 0, sql.ErrNoRows;
+	} else if err != nil {
+		return 0, err;
+	}
+
+	return id, nil;
+}
+
+func (s *movementStore) DeleteById(tx *sql.Tx, id int) error {
+	row, err := s.Exec("DELETE FROM movements WHERE movements.id = $1 RETURNING movements.id", id);
+	if err != nil {
+		return err;
+	}
+
+	if r, err := row.RowsAffected(); err != nil {
+		return err;
+	} else if r == 0 {
+		return sql.ErrNoRows;
+	}
+
+	return nil;
+}
---
+++ api/stores/stores.go
@@ -0,0 +1,27 @@
+package stores
+
+import "database/sql"
+
+type Stores struct {
+	DB *sql.DB
+	Movement *movementStore
+}
+
+func New(db *sql.DB) *Stores {
+	return &Stores{
+		DB: db,
+		Movement: &movementStore{db},
+	}
+}
+
+func (s *Stores) Begin() (*sql.Tx, error) {
+	return s.DB.Begin();
+}
+
+func (s *Stores) Commit(tx *sql.Tx) error {
+	return tx.Commit();
+}
+
+func (s *Stores) Rollback(tx *sql.Tx) error {
+	return tx.Rollback();
+}
