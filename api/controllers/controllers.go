c5c143a895da0728b357d261fe3cef2c0b4d4d23
dzeleniak <zeleniak.david@gmail.com>
2023-09-26 13:08:01 -0700

    register movement api routes

--- api/controllers/controllers.go
+++ api/controllers/controllers.go
@@ -6,12 +6,12 @@ import (
 	"github.com/labstack/echo/v4"
 )
 
-type Handlers struct {
+type Controllers struct {
 	MovementController
 }
 
-func New(s *services.Services) *Handlers {
-	return &Handlers{
+func New(s *services.Services) *Controllers {
+	return &Controllers{
 		&movementController{s.Movement},
 	}
 }
@@ -21,10 +21,20 @@ func Echo() *echo.Echo {
 
 	e.Use(middleware.Logger());
 	e.Use(middleware.Recover());
-	e.Pre(middleware.RemoveTrailingSlash);
+	e.Pre(middleware.RemoveTrailingSlash());
 	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
 		AllowedOrigins: []string{"*"},
 	}))
 
 	return e;
 }	
+
+func SetApi(e *echo.Echo, c *Controllers, m echo.MiddlewareFunc) {
+	g := e.Group("/api/v1")
+	g.Use(m)
+
+	g.GET("/movements", c.MovementController.GetMovements)
+	g.POST("/movements", c.MovementController.CreateMovement)
+	g.PUT("/movements", c.MovementController.UpdateMovementById)
+	g.DELETE("/movements/:id", c.MovementController.DeleteMovementById)
+}
