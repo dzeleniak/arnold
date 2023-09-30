1cb8d2e8a012ab8e8354563b3cf724dca0555f20
dzeleniak <zeleniak.david@gmail.com>
2023-09-30 12:46:14 -0700

    Add MovementSet Controller Support

--- api/controllers/controllers.go
+++ api/controllers/controllers.go
@@ -1,17 +1,21 @@
 package controllers
 
 import (
+	"net/http"
+
 	"github.com/dzeleniak/arnold/services"
 	"github.com/labstack/echo/v4"
 )
 
 type Controllers struct {
 	MovementController
+	MovementSetController
 }
 
 func New(s *services.Services) *Controllers {
 	return &Controllers{
-		&movementController{s.Movement},
+		MovementController: &movementController{s.Movement},
+		MovementSetController: &movementSetController{s.MovementSet},
 	}
 }
 
@@ -20,13 +24,27 @@ func Echo() *echo.Echo {
 
 	return e;
 }	
+func SetDefault(e *echo.Echo) {
+	e.GET("/", func(ctx echo.Context) error {
+		return ctx.JSON(http.StatusOK, "Success!");
+	})
+}
 
 func SetApi(e *echo.Echo, c *Controllers, m echo.MiddlewareFunc) {
 	g := e.Group("/api/v1")
-	g.Use(m)
+	// g.Use(m)
+
+	g.GET("/", func (ctx echo.Context) error {
+		return ctx.JSON(http.StatusOK, "API V1")
+	})
 
 	g.GET("/movements", c.MovementController.GetMovements)
 	g.POST("/movements", c.MovementController.CreateMovement)
 	g.PUT("/movements", c.MovementController.UpdateMovementById)
 	g.DELETE("/movements/:id", c.MovementController.DeleteMovementById)
+
+	g.GET("/sets", c.MovementSetController.GetMovementSets);
+	g.POST("/sets", c.MovementSetController.CreateMovementSet);
+	g.PUT("/sets", c.MovementSetController.UpdateMovementSet);
+	g.DELETE("/sets/:id", c.MovementSetController.DeleteMovementSet);
 }
---
+++ api/controllers/movementSet.go
@@ -0,0 +1,84 @@
+package controllers
+
+import (
+	"net/http"
+	"strconv"
+
+	"github.com/dzeleniak/arnold/models"
+	"github.com/dzeleniak/arnold/services"
+	"github.com/labstack/echo/v4"
+)
+
+type (
+	MovementSetController interface {
+		GetMovementSets(ctx echo.Context) error
+		CreateMovementSet(ctx echo.Context) error
+		UpdateMovementSet(ctx echo.Context) error
+		DeleteMovementSet(ctx echo.Context) error
+	}	
+
+	movementSetController struct {
+		services.MovementSetService
+	}
+)
+
+func (c *movementSetController) GetMovementSets(ctx echo.Context) error {
+	var sets []models.MovementSet
+	sets, err := c.MovementSetService.GetMovementSets()
+		
+	if err != nil {
+		return ctx.JSON(http.StatusInternalServerError, err.Error())
+	}
+
+	return ctx.JSON(http.StatusOK, sets)
+}
+
+func (c *movementSetController) CreateMovementSet(ctx echo.Context) error {
+	var set *models.MovementSet
+
+	err := ctx.Bind(&set)
+
+	if err != nil {
+		return ctx.JSON(http.StatusBadRequest, err.Error())	
+	}
+
+	id, err := c.MovementSetService.CreateMovementSet(set)
+
+	if err != nil {
+		return ctx.JSON(http.StatusInternalServerError, err.Error())
+	}
+
+	return ctx.JSON(http.StatusOK, id)
+}
+
+func (c *movementSetController) UpdateMovementSet(ctx echo.Context) error {
+
+	var set *models.MovementSet
+
+	if	err := ctx.Bind(&set); err != nil {
+		return ctx.JSON(http.StatusBadRequest, err.Error())
+	}
+
+	id, err := c.MovementSetService.UpdateMovementSet(set);
+
+	if err != nil {
+		return ctx.JSON(http.StatusInternalServerError, err.Error())
+	}
+
+	return ctx.JSON(http.StatusOK, id);
+}
+
+func (c *movementSetController) DeleteMovementSet(ctx echo.Context) error {
+
+	id, err := strconv.Atoi(ctx.Param("id"));
+
+	if err != nil {
+		return ctx.JSON(http.StatusBadRequest, err.Error())
+	}
+
+	if err := c.MovementSetService.DeleteMovementSet(int64(id)); err != nil {
+		return ctx.JSON(http.StatusInternalServerError, err.Error())
+	}
+
+	return ctx.JSON(http.StatusOK, id)
+}
